524831e8d0a3fa1ed9ab45baf1bbffedccfc7343
Leonardo Nonino <lnonino92@gmail.com>
2025-06-23 13:09:12 -0300

    entrega alfa empezando recuperar contreaseña

--- js/inicio.js
+++ js/inicio.js
@@ -1,11 +1,11 @@
 const usuarios = [
-    {nombre: "Leonardo", nombreUsuario: "36765496", contrasena: "starplatinum", ahorros: 265000, numeroCcomitente: 54687879},
-    {nombre: "Diego", nombreUsuario: "diego", contrasena: "brando", ahorros: 295000, numeroCcomitente: 54687880},
-    {nombre: "Pablo", nombreUsuario: "pablin", contrasena: "guitarra", ahorros: 25000, numeroCcomitente: 54687881},
-    {nombre: "Alejandro", nombreUsuario: "ale", contrasena: "discordia", ahorros: 465000, numeroCcomitente: 54687882},
-    {nombre: "Santiago", nombreUsuario: "santi", contrasena: "martin", ahorros: 65000, numeroCcomitente: 54687883},
-    {nombre: "Juan", nombreUsuario: "juan", contrasena: "pablo", ahorros: 5000, numeroCcomitente: 54687884},
-    {nombre: "Gabriel", nombreUsuario: "gabi", contrasena: "sosa", ahorros: 635000, numeroCcomitente: 54687885,}
+    {nombre: "Leonardo", nombreUsuario: "36765496", contrasena: "starplatinum", ahorros: 265000, numeroCcomitente: 54687879, palabraSecreta: "Fantástico"},
+    {nombre: "Diego", nombreUsuario: "diego", contrasena: "brando", ahorros: 295000, numeroCcomitente: 54687880, palabraSecreta: "Financiera"},
+    {nombre: "Pablo", nombreUsuario: "pablin", contrasena: "guitarra", ahorros: 25000, numeroCcomitente: 54687881, palabraSecreta: "8Mile"},
+    {nombre: "Alejandro", nombreUsuario: "ale", contrasena: "discordia", ahorros: 465000, numeroCcomitente: 54687882, palabraSecreta: "Desodorante"},
+    {nombre: "Santiago", nombreUsuario: "santi", contrasena: "martin", ahorros: 65000, numeroCcomitente: 54687883, palabraSecreta: "Octógono"},
+    {nombre: "Juan", nombreUsuario: "juan", contrasena: "pablo", ahorros: 5000, numeroCcomitente: 54687884, palabraSecreta: "Terraforma"},
+    {nombre: "Gabriel", nombreUsuario: "gabi", contrasena: "sosa", ahorros: 635000, numeroCcomitente: 54687885, palabraSecreta: "Ascención"}
 ]
 
 let usuariosJSON = JSON.stringify(usuarios)
@@ -56,7 +56,10 @@ const ingreso = document.getElementById("ingresoBoton")
     })
 
 
-
+const ContrasenaOlvidada = document.getElementById("contrasenaOlvidada")
+ContrasenaOlvidada.addEventListener("click", () => {
+    window.location.href = "./recuperacion"
+})
 
 
 
---
+++ js/recuperarContrasena.js
@@ -0,0 +1,6 @@
+const nombreUsuario = document.getElementById("nombreRecuperacion")
+const palabraSecreta = document.getElementById("palabraSecreta")
+const NuevaContrasena = document.getElementById("nuevaContrasena")
+const RepetirNuevaContrasena = document.getElementById("repetirNuevaContrasena")
+
+
